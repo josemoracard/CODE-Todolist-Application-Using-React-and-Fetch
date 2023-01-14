a0d4631697bafb127cbf2cbc3e88fcf734004afb
josemoracard <jose.cardinales@gmail.com>
2023-01-14 02:04:30 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from 'react';
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [hoverIndex, setHoverIndex] = useState(null);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const task = newTask.trim();
+    if(task){
+      setTasks([...tasks, task]);
+      setNewTask('');
+    }
+  }
+
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <div>
+      <div id="container">
+      <h1 id="title">Todos</h1>
+      <div id='container-body'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="What needs to be done?"
+        />
+      </form>
+      
+      <ul>
+      {tasks.length === 0 && <p>No tasks, add a task</p>}
+        {tasks.map((task, index) => (
+          <li key={index} onMouseEnter={() => setHoverIndex(index)} onMouseLeave={() => setHoverIndex(null)}>
+            {task}
+            {hoverIndex === index && <span onClick={() => handleDelete(index)}><i className="fa-solid fa-x"></i></span>}
+          </li>
+        ))}
+      </ul>
+      {tasks.length > 0 && <p id='tasks-left'>Number of tasks: {tasks.length}</p>}
+      </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home;
